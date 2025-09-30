db4211b51e1c9e13f74b21294a5be3097eec3a77
PrabathVIJA <prabath.kool@gmail.com>
2025-09-30 18:00:09 +0530

    uncommented test methods in LoginRegister Test

--- src/test/java/org/automation/LoginRegisterTest.java
+++ src/test/java/org/automation/LoginRegisterTest.java
@@ -24,88 +24,88 @@ public class LoginRegisterTest extends BaseTest {
 
 	}
 
-//	@Test
-//	public void validate_Without_userName() {
-//		goToAccountPage();
-//		loginRegister = new LoginRegisterPage(driver);
-//		loginRegister.loginClick();
-//		String actual = loginRegister.userNameRequiredText();
-//
-//		Assert.assertEquals(actual, "Error: Username is required.");
-//
-//	}
-//
-//	@Test
-//	public void validate_With_EmptyPasswordField() {
-//		goToAccountPage();
-//		loginRegister = new LoginRegisterPage(driver);
-//
-//		loginRegister.enterUsernameOrEmail("pra");
-//		loginRegister.loginClick();
-//		String actual = loginRegister.emptyPasswordFieldText();
-//
-//		Assert.assertEquals(actual, "Error: The password field is empty.");
-//
-//	}
-//
-//	@Test
-//	public void validate_WronginputEntered_In_UserNameField() {
-//		goToAccountPage();
-//		loginRegister = new LoginRegisterPage(driver);
-//		loginRegister.enterUsernameOrEmail("pra");
-//		String enteredValue = loginRegister.getInputEnteredInUserNameOrEmailAddressField();
-//		Assert.assertEquals(enteredValue, "pra");
-//	}
-//
-//	@Test
-//	public void validate_WronginputEntered_In_PWDField() {
-//		goToAccountPage();
-//		loginRegister = new LoginRegisterPage(driver);
-//		loginRegister.enterPassword("prabath@");
-//		String enteredValue = loginRegister.getInputEnteredInPasswordField();
-//		Assert.assertEquals(enteredValue, "prabath@");
-//	}
-//
-//	@Test
-//	public void validate_LostYourPasswordBtn() {
-//		goToAccountPage();
-//		loginRegister = new LoginRegisterPage(driver);
-//		loginRegister.clickLostMyPassword();
-//		String val = loginRegister.getLostYourPWDTextOnPWDChangePage();
-//		Assert.assertEquals(val,
-//				"Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.");
-//
-//	}
-//
-//	@Test
-//	public void loginWithValidCredentials() {
-//		goToAccountPage();
-//		loginRegister = new LoginRegisterPage(driver);
-//		loginRegister.enterUsernameOrEmail(ConfigLoader.getInstance().getUserName());
-//		loginRegister.enterPassword(ConfigLoader.getInstance().getPassword());
-//		loginRegister.loginClick();
-//
-//		Assert.assertEquals(loginRegister.greetingMessage(), "Hello kiran (not kiran? Log out)");
-//		loginRegister.clickLogOut();
-//	}
-//
-//	@Test
-//	public void openAboutPageInNewTabAndSwitchToIt() {
-//		goToAboutPage();
-//		HomePage home = new HomePage(driver);
-//
-//		Assert.assertEquals(home.clickAboutLinkOpenInNewTabAndSwitchToIt(), "About – AskOmDch");
-//
-//	}
-//
-//	// check the background color of login button
-//	@Test
-//	public void loginBtn_BackGroundColor_validation() {
-//		goToAccountPage();
-//		loginRegister = new LoginRegisterPage(driver);
-//
-//		Assert.assertEquals(loginRegister.loginBtn_BackGround_Color("background-color"), "rgba(49, 151, 214, 1)");
-//
-//	}
+	@Test
+	public void validate_Without_userName() {
+		goToAccountPage();
+		loginRegister = new LoginRegisterPage(driver);
+		loginRegister.loginClick();
+		String actual = loginRegister.userNameRequiredText();
+
+		Assert.assertEquals(actual, "Error: Username is required.");
+
+	}
+
+	@Test
+	public void validate_With_EmptyPasswordField() {
+		goToAccountPage();
+		loginRegister = new LoginRegisterPage(driver);
+
+		loginRegister.enterUsernameOrEmail("pra");
+		loginRegister.loginClick();
+		String actual = loginRegister.emptyPasswordFieldText();
+
+		Assert.assertEquals(actual, "Error: The password field is empty.");
+
+	}
+
+	@Test
+	public void validate_WronginputEntered_In_UserNameField() {
+		goToAccountPage();
+		loginRegister = new LoginRegisterPage(driver);
+		loginRegister.enterUsernameOrEmail("pra");
+		String enteredValue = loginRegister.getInputEnteredInUserNameOrEmailAddressField();
+		Assert.assertEquals(enteredValue, "pra");
+	}
+
+	@Test
+	public void validate_WronginputEntered_In_PWDField() {
+		goToAccountPage();
+		loginRegister = new LoginRegisterPage(driver);
+		loginRegister.enterPassword("prabath@");
+		String enteredValue = loginRegister.getInputEnteredInPasswordField();
+		Assert.assertEquals(enteredValue, "prabath@");
+	}
+
+	@Test
+	public void validate_LostYourPasswordBtn() {
+		goToAccountPage();
+		loginRegister = new LoginRegisterPage(driver);
+		loginRegister.clickLostMyPassword();
+		String val = loginRegister.getLostYourPWDTextOnPWDChangePage();
+		Assert.assertEquals(val,
+				"Lost your password? Please enter your username or email address. You will receive a link to create a new password via email.");
+
+	}
+
+	@Test
+	public void loginWithValidCredentials() {
+	goToAccountPage();
+		loginRegister = new LoginRegisterPage(driver);
+		loginRegister.enterUsernameOrEmail(ConfigLoader.getInstance().getUserName());
+		loginRegister.enterPassword(ConfigLoader.getInstance().getPassword());
+		loginRegister.loginClick();
+
+		Assert.assertEquals(loginRegister.greetingMessage(), "Hello kiran (not kiran? Log out)");
+		loginRegister.clickLogOut();
+	}
+
+	@Test
+	public void openAboutPageInNewTabAndSwitchToIt() {
+		goToAboutPage();
+		HomePage home = new HomePage(driver);
+
+		Assert.assertEquals(home.clickAboutLinkOpenInNewTabAndSwitchToIt(), "About – AskOmDch");
+
+	}
+
+	// check the background color of login button
+	@Test
+	public void loginBtn_BackGroundColor_validation() {
+		goToAccountPage();
+		loginRegister = new LoginRegisterPage(driver);
+
+		Assert.assertEquals(loginRegister.loginBtn_BackGround_Color("background-color"), "rgba(49, 151, 214, 1)");
+
+	}
 
 }
